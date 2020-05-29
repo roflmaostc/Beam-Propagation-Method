fe1ffebf451f184a9f9cf4634ffa7af3f14d3b06
roflmaostc <f.wechsler@tum.de>
2020-05-29 10:41:49 +0200

    Initial add of the raw code

---
+++ beam_propagation_method.py
@@ -0,0 +1,98 @@
+'''Homework 2, Computational Photonics, SS 2020:  Beam propagation method.
+'''
+import numpy as np
+import scipy.sparse as sps
+
+
+def waveguide(xa, xb, Nx, n_cladding, n_core):
+    '''Generates the refractive index distribution of a slab waveguide
+    with step profile centered around the origin of the coordinate
+    system with a refractive index of n_core in the waveguide region
+    and n_cladding in the surrounding cladding area.
+    All lengths have to be specified in µm.
+
+    Parameters
+    ----------
+        xa : float
+            Width of calculation window
+        xb : float
+            Width of waveguide
+        Nx : int
+            Number of grid points
+        n_cladding : float
+            Refractive index of cladding
+        n_core : float
+            Refractive index of core
+
+    Returns
+    -------
+        n : 1d-array
+            Generated refractive index distribution
+        x : 1d-array
+            Generated coordinate vector
+    '''
+    pass
+
+
+
+
+def gauss(xa, Nx, w):
+    '''Generates a Gaussian field distribution v = exp(-x^2/w^2) centered
+    around the origin of the coordinate system and having a width of w.
+    All lengths have to be specified in µm.
+
+    Parameters
+    ----------
+        xa : float
+            Width of calculation window
+        Nx : int
+            Number of grid points
+        w  : float
+            Width of Gaussian field
+
+    Returns
+    -------
+        v : 1d-array
+            Generated field distribution
+        x : 1d-array
+            Generated coordinate vector
+    '''
+    pass
+
+
+
+
+def beamprop_CN(v_in, lam, dx, n, nd,  z_end, dz, output_step):
+    '''Propagates an initial field over a given distance based on the
+    solution of the paraxial wave equation in an inhomogeneous
+    refractive index distribution using the explicit-implicit
+    Crank-Nicolson scheme. All lengths have to be specified in µm.
+
+    Parameters
+    ----------
+        v_in : 1d-array
+            Initial field
+        lam : float
+            Wavelength
+        dx : float
+            Transverse step size
+        n : 1d-array
+            Refractive index distribution
+        nd : float
+            Reference refractive index
+        z_end : float
+            Propagation distance
+        dz : float
+            Step size in propagation direction
+        output_step : int
+            Number of steps between field outputs
+
+    Returns
+    -------
+        v_out : 2d-array
+            Propagated field
+        z : 1d-array
+            z-coordinates of field output
+    '''
+    pass
+
